354e0a830472ca24b4fbf02c1deaf523f3cfa50b
mpokropowicz <pokropowicz.shinto+git@gmail.com>
2017-02-22 13:30:43 -0500

    first commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+gem "sinatra"
+gem "minitest"
+gem "rack"
+gem "rack-test"
---
+++ tests/test_app.rb
@@ -0,0 +1,7 @@
+require "minitest/autorun"
+require "rack/test"
+require_relative "../app.rb"
+
+class TestApp < Minitest::Test 
+	include Rack::Test::Methods
+end
